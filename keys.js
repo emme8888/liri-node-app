169d4abdd3fba60ece2be51a57f28fd388b97c41
emme8888 <emme8888@hotmail.com>
2018-08-10 22:53:44 -0500

    homework

---
+++ keys.js
@@ -0,0 +1,22 @@
+// console.log('this is loaded')
+
+// spotify 
+var spotifyKeys={
+
+ID: "0e29d01e80694a3d8e0d599734710f67",
+Secret:"80229fd382bd4e0192addcda262429fa"
+}
+
+exports.twitterKeys = {
+ consumer_key:'q1fd350geAgSv6ybPFOh9TWUO',
+
+consumer_secret: '4YjdUtVQQ1etjGJchQbEDLzfen8KtrLWIOeRZKsG1lAmWCUtp3',
+
+access_token_key:'1025573841656066048-lRxUhltnQLctqFUvecnSzoWze7R3tx',
+
+access_token_secret: 'Ri7gGtLxY475hs4a0n3jbSsbWzEKQlBDs6X9GPmhziC1G',
+}
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys
+}
